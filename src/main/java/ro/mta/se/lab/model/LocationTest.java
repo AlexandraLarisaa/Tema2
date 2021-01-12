ea501a338e58b99c4fdbe7772c0df7e62241cd08
alexandra-oras-selab <larisa.oras@mta.ro>
2021-01-12 14:17:06 +0200

    Actualizare set de teste in cadrul clasei LocationTest
    Am adaugat un nou set de teste in cadrul clasei LocationTest.
    
    Signed-off-by: alexandra-oras-selab <larisa.oras@mta.ro>

--- src/main/java/ro/mta/se/lab/model/LocationTest.java
+++ src/main/java/ro/mta/se/lab/model/LocationTest.java
@@ -4,25 +4,36 @@ import org.junit.*;
 
 public class LocationTest {
     private Location location;
+    private Location location1;
 
     @Before
-    public void init(){
-        location = new Location("RO","Bacau","123","123","250");
+    public void init() {
+        location = new Location("RO", "Bacau", "123", "123", "250");
+        location1 = new Location("IT", "Roma", "456", "456", "260");
+
     }
+
     @Test
     public void getCountry() {
         Assert.assertEquals("RO", location.getCountry());
+        Assert.assertEquals("IT", location1.getCountry());
     }
 
     @Test
     public void getCity() {
         Assert.assertEquals("Bacau", location.getCity());
+        Assert.assertEquals("Roma", location1.getCity());
+
     }
 
     @Test
     public void getLatitude() {
         String latitude = location.getLatitude();
-        if(!latitude.equals("123")){
+        if (!latitude.equals("123")) {
+            Assert.fail();
+        }
+        String latitude1 = location1.getLatitude();
+        if (!latitude1.equals("456")) {
             Assert.fail();
         }
     }
@@ -30,7 +41,11 @@ public class LocationTest {
     @Test
     public void getLongitude() {
         String longitude = location.getLongitude();
-        if(!longitude.equals("123")){
+        if (!longitude.equals("123")) {
+            Assert.fail();
+        }
+        String longitude1 = location1.getLongitude();
+        if (!longitude1.equals("456")) {
             Assert.fail();
         }
     }
@@ -38,5 +53,6 @@ public class LocationTest {
     @Test
     public void getID() {
         Assert.assertEquals("250", location.getID());
+        Assert.assertEquals("260", location1.getID());
     }
 }
